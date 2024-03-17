8bfd51353ab1540253f05be706bbb14f61b28fb6
manoj <gandhamanojkutty@gmail.com>
2024-03-17 11:03:56 +0530

    code updated

--- main.tf
+++ main.tf
@@ -40,4 +40,5 @@ resource "aws_route" "igw" {
   count = length(var.subnets["pubilc"].route_table_ids)
   route_table_id = module.subnets["pubilc"].route_table_ids[count.index]
   gateway_id = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
 }
