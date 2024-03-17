f2fc050bfb5c3f0c4640f19eef0f6c762f951f85
manoj <gandhamanojkutty@gmail.com>
2024-03-17 11:01:46 +0530

    code updated

--- main.tf
+++ main.tf
@@ -35,3 +35,9 @@ resource "aws_nat_gateway" "ngw" {
 
   tags = merge(var.tags, { Name = "${var.env}-ngw" })
 }
+
+resource "aws_route" "igw" {
+  count = length(var.subnets["pubilc"].route_table_ids)
+  route_table_id = module.subnets["pubilc"].route_table_ids[count.index]
+  gateway_id = aws_internet_gateway.igw.id
+}
