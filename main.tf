347ee74e6239c690b6ba24d0e56098851d02d733
manoj <gandhamanojkutty@gmail.com>
2024-03-19 22:24:36 +0530

    code updated

--- main.tf
+++ main.tf
@@ -49,7 +49,3 @@ resource "aws_route" "ngw" {
   nat_gateway_id = element(aws_nat_gateway.ngw.*.id, count.index)
   destination_cidr_block = "0.0.0.0/0"
 }
-
-output "subnets" {
-  value = module.subnets
-}
